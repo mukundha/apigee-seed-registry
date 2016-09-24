b3ad824dc45674a2897ab16f66b08daa7b938271
mukundha@apigee.com <mukundha@apigee.com>
2016-09-24 14:30:44 -0400

    update config sample

--- config/config-sample.js
+++ config/config-sample.js
@@ -4,15 +4,10 @@ module.exports = {
     "baasBaseURL" : "your-baas-base-url",
     "baasOrg" : "your-baas-org",
     "baasApp" : "your-baas-app",
-    "dockerHost": "your-docker-host",
-	"dockerPort": "docker-port",
-	"dockerCaFile": "your docker ca.pem",
-	"dockerCertFile": "your docker cert.pem",
-	"dockerKeyFile": "docker machine key.pem",
-	"dockerPrefix": "apigeeseed/",
+    "dockerPrefix": "apigeeseed/",
 	"dockerAuthConfig": {
-    			"username": "dockerhub-user",
-    			"password": "dockerhub-password",
-    			"email": "dockerhub-email",
+    			"username": "_json_key",
+    			"password": "<key.json contents>",
+    			"email": "1234@5678.com",
 	}
 };
